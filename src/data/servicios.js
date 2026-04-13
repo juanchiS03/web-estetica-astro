e008f0b7e15bd94fb494ef62d9631a4ce4637bb2
juanchiS03 <juansanchezdecorta@gmail.com>
2026-04-13 10:08:59 +0200

    Servicios

---
+++ src/data/servicios.js
@@ -0,0 +1,23 @@
+export const serviciosData = [
+  {
+    slug: 'depilacion-laser',
+    titulo: 'Depilación por Laser',
+    descripcion: 'Tratamiento efectivo para eliminar el vello no deseado de forma permanente.',
+    precios: [
+        { nombre: "Piernas Completas", precio: "80€", detalle: "Sesión de 30 minutos" },
+        { nombre: "Axilas", precio: "30€", detalle: "Sesión de 15 minutos" },
+        { nombre: "Bikini", precio: "50€", detalle: "Sesión de 20 minutos" }
+    ]
+  },
+  {
+    slug: 'masajes-spa',
+    titulo: 'Masajes & SPA',
+    descripcion: 'Relajación total para cuerpo y mente en un ambiente único.',
+    precios: [
+      { nombre: "Masaje Relajante", precio: "40€", detalle: "Sesión de 50 minutos con aceites" },
+      { nombre: "Piedras Volcánicas", precio: "55€", detalle: "Terapia de calor profundo" },
+      { nombre: "Descontracturante", precio: "45€", detalle: "Ideal para liberar tensiones" }
+    ]
+  }
+
+];
