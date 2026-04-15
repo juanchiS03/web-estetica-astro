9c457bea9bc4d635b67b8e5b37fe6f69a43b2c4f
juanchiS03 <juansanchezdecorta@gmail.com>
2026-04-15 18:14:36 +0200

    configuracion sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; // 1. Importamos el sitemap
 
 // https://astro.build/config
 export default defineConfig({
+  // 2. IMPORTANTE: Cambia esta URL por la tuya de Vercel
+  site: 'https://esteticasandramzcordoba.vercel.app/', 
+  
+  integrations: [
+    sitemap(), // 3. Añadimos la integración aquí
+  ],
+
   vite: {
     plugins: [tailwindcss()]
   }
